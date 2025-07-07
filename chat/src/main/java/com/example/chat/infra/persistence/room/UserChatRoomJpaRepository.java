aa0a65331c831b37753ef85e12a79acb0254a8cf
kimc <tjdtlr12349@gmail.com>
2025-07-08 00:14:31 +0900

    UserChatRoomJpaRepository의 ID 타입을 UserChatRoomKey에서 Long으로 변경

--- chat/src/main/java/com/example/chat/infra/persistence/room/UserChatRoomJpaRepository.java
+++ chat/src/main/java/com/example/chat/infra/persistence/room/UserChatRoomJpaRepository.java
@@ -1,10 +1,10 @@
 package com.example.chat.infra.persistence.room;
 
-import com.example.chat.dto.room.UserChatRoomKey;
-import com.example.chat.infra.entity.room.UserChatRoomEntity;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.chat.infra.entity.room.UserChatRoomEntity;
+
 @Repository
-public interface UserChatRoomJpaRepository extends R2dbcRepository<UserChatRoomEntity, UserChatRoomKey> {
+public interface UserChatRoomJpaRepository extends R2dbcRepository<UserChatRoomEntity, Long> {
 }
